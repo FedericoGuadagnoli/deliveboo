9ff82693d3e957f1285cab09a9eb87073e817f15
Alberto-Bruno <brunoalberto97@outlook.it>
2023-04-03 22:34:39 +0200

    Menu dishes card

--- resources/views/auth/dishes/index.blade.php
+++ resources/views/auth/dishes/index.blade.php
@@ -1,11 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($dishes as $dish)
-        <ul>
-            <li>
-                <h1>{{ $dish->name }}</h1>
-            </li>
-        </ul>
-    @endforeach
+    <div class="container">
+        @foreach ($dishes as $dish)
+            <div class="row d-flex dashboard justify-content-around">
+                <div class="col-4">
+                    <div class="card">
+                        <img src="https://www.frescadelivery.com/wp-content/uploads/2020/06/Antipasto-misto-for-2-1-scaled-e1619175792723.jpg"
+                            alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $dish->name }}</h5>
+                            <p class="card-text">{{ $dish->description }}</p>
+                            <p><strong>€ {{ $dish->price }}</strong></p>
+                            <div class="d-flex justify-content-around">
+                                <a href="" class="btn btn-outline-primary"><i class="fa-solid fa-eye"></i>
+                                    Osserva
+                                </a>
+                                <a href="" class="btn btn-outline-success"><i class="fa-solid fa-pencil"></i>
+                                    Modifica
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
