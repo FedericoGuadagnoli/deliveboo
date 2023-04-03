33396029f08c3cb8ebb45f62f3371f253d918023
AndreaMinelli <minelliandrea94@gmail.com>
2023-04-03 15:39:54 +0200

    Created store function in DishController

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Dish;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class DishController extends Controller
 {
@@ -33,6 +34,16 @@ class DishController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validation($request);
+        $data = $request->all();
+        $restaurant_id = Auth::user()->restaurant->id;
+        $dish = new Dish();
+        $dish->fill($data);
+        $dish->slug = Str::slug($data['name'], '-');
+        $dish->restaurant_id = $restaurant_id;
+        $dish->save();
+
+        return to_route('auth.dishes.show', $dish->id);
     }
 
     /**
@@ -40,7 +51,7 @@ class DishController extends Controller
      */
     public function show(Dish $dish)
     {
-        //
+        return view('auth.dishes.show', compact('dish'));
     }
 
     /**
@@ -48,7 +59,6 @@ class DishController extends Controller
      */
     public function edit(Dish $dish)
     {
-        //
     }
 
     /**
@@ -65,5 +75,33 @@ class DishController extends Controller
     public function destroy(Dish $dish)
     {
         //
+    }
+
+    private function validation(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => 'string|required|min:5',
+                'description' => 'string|required|min:5',
+                'price' => 'required|numeric|min:0.1|max:150',
+                'availability' => 'nullable|boolean',
+                'image' => 'nullable|image|mimes:jpeg,jpg,svg,png',
+            ],
+            [
+                'name.required' => 'Devi inserire un nome.',
+                'name.string' => 'Il nome che hai inserito non è valido.',
+                'name.min' => 'Il nome deve avere minimo :min caratteri.',
+                'description.required' => 'Devi inserire una descrizione.',
+                'description.string' => 'La descrizione non è valida.',
+                'description.min' => 'La descrizione deve avere minimo :min caratteri.',
+                'price.required' => 'Il prezzo è obbligatorio.',
+                'price.numeric' => 'Il prezzo inserito non è valido.',
+                'price.min' => 'Il prezzo minimo è di :min €.',
+                'price.max' => 'Il prezzo massimo è di :max €.',
+                'availability.boolean' => 'Il valore inserito per la disponibilità non è valido.',
+                'image.mimes' => 'Le estensioni valide per le immagini sono :mimes.',
+                'image.image' => 'L\'immagine deve essere un\'immagine valida.',
+            ]
+        );
     }
 }
