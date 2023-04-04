d56f888f61ce1e681f054b96065cc2136d24af1a
Alberto-Bruno <brunoalberto97@outlook.it>
2023-04-04 16:28:38 +0200

    edits-index

--- resources/views/auth/dishes/index.blade.php
+++ resources/views/auth/dishes/index.blade.php
@@ -2,15 +2,18 @@
 
 @section('content')
     <div class="container py-4">
-    
+        <h1 class="text-center text-white">Lista Piatti</h1>
         <div class="row dashboard justify-content-around">
-            <a href="{{ route('admin.dishes.create') }}" class="btn btn-danger w-75"><i
-                                class="fa-solid fa-plus me-2"></i>Aggiungi Piatto</a>
+            <div class="d-flex justify-content-end">
+                <a href="{{ route('admin.dishes.create') }}" class="btn btn-success"><i
+                        class="fa-solid fa-plus me-2"></i>Aggiungi
+                    Piatto</a>
+            </div>
             @foreach ($dishes as $dish)
-                <div class="col-4">
-                    
-                    <div class="card my-4">
-                        <img src="{{ asset('storage/' . $dish->image) }}" alt="{{ $dish->slug }}">
+                <div class="col-4 p-4">
+
+                    <div class="card">
+                        <img class="image-card" src="{{ asset('storage/' . $dish->image) }}" alt="{{ $dish->slug }}">
                         <div class="card-body">
                             <h5 class="card-title">{{ $dish->name }}</h5>
                             <p class="card-text">{{ $dish->description }}</p>
@@ -33,7 +36,7 @@
                                         Elimina
                                     </button>
                                 </form>
-                                
+
                             </div>
                         </div>
                     </div>
