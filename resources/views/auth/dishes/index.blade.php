c9014d961f283a55ab0c1431200114e244ce20e0
AndreaMinelli <minelliandrea94@gmail.com>
2023-04-04 11:46:07 +0200

    Added storage path for dish image

--- resources/views/auth/dishes/index.blade.php
+++ resources/views/auth/dishes/index.blade.php
@@ -2,40 +2,43 @@
 
 @section('content')
     <div class="container py-4">
-        
-            <div class="row dashboard justify-content-around">
-                @foreach ($dishes as $dish)
-                    <div class="col-4">
-                        <div class="card my-4">
-                            <img src="https://www.frescadelivery.com/wp-content/uploads/2020/06/Antipasto-misto-for-2-1-scaled-e1619175792723.jpg"
-                                alt="">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $dish->name }}</h5>
-                                <p class="card-text">{{ $dish->description }}</p>
-                                <p><strong>€ {{ $dish->price }}</strong></p>
-                                <div class="d-flex justify-content-around">
-                                    <a href="{{ route('admin.dishes.show', $dish->id) }}" class="btn btn-outline-primary"><i class="fa-solid fa-eye"></i>
-                                        Osserva
-                                    </a>
-                                    <a href="{{ route('admin.dishes.edit', $dish->id) }}" class="btn btn-outline-warning"><i class="fa-solid fa-pencil"></i>
-                                        Modifica
-                                    </a>
-                                    <form class="delete-form d-inline"  action="{{route("admin.dishes.destroy", $dish->id)}}" method="POST">
-                                        @csrf
-                                        @method("DELETE")
-                                        <button type="submit" class="btn btn-outline-danger"><i class="fa-solid fa-trash-can"></i>
-                                            Elimina
-                                        </button>
-                                    </form>
-                                    {{-- <a href="" class="btn btn-outline-danger"><i class="fa-solid fa-trash-can"></i>
+
+        <div class="row dashboard justify-content-around">
+            @foreach ($dishes as $dish)
+                <div class="col-4">
+                    <div class="card my-4">
+                        <img src="{{ asset('storage/' . $dish->image) }}" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $dish->name }}</h5>
+                            <p class="card-text">{{ $dish->description }}</p>
+                            <p><strong>€ {{ $dish->price }}</strong></p>
+                            <div class="d-flex justify-content-around">
+                                <a href="{{ route('admin.dishes.show', $dish->id) }}" class="btn btn-outline-primary"><i
+                                        class="fa-solid fa-eye"></i>
+                                    Osserva
+                                </a>
+                                <a href="{{ route('admin.dishes.edit', $dish->id) }}" class="btn btn-outline-warning"><i
+                                        class="fa-solid fa-pencil"></i>
+                                    Modifica
+                                </a>
+                                <form class="delete-form d-inline" action="{{ route('admin.dishes.destroy', $dish->id) }}"
+                                    method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-outline-danger"><i
+                                            class="fa-solid fa-trash-can"></i>
+                                        Elimina
+                                    </button>
+                                </form>
+                                {{-- <a href="" class="btn btn-outline-danger"><i class="fa-solid fa-trash-can"></i>
                                         Elimina
                                     </a> --}}
-                                </div>
                             </div>
                         </div>
                     </div>
-                @endforeach
-            </div>
-        
+                </div>
+            @endforeach
+        </div>
+
     </div>
 @endsection
