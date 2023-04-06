b9e04b04b5865d8cbfc15ce385936c1aba465a28
AndreaMinelli <minelliandrea94@gmail.com>
2023-04-06 11:16:15 +0200

    Added margin into generic_alert

--- resources/views/includes/alerts/generic_alert.blade.php
+++ resources/views/includes/alerts/generic_alert.blade.php
@@ -7,7 +7,7 @@
         </ul>
     </div>
 @elseif (session('msg'))
-    <div class="alert alert-{{ session('type') }}" role="alert">
+    <div class="alert alert-{{ session('type') }} mt-5" role="alert">
         {{ session('msg') }}
     </div>
 @endif
