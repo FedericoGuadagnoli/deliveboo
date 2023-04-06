621edad461277dca0018616d52b46de9cf81025c
AndreaMinelli <minelliandrea94@gmail.com>
2023-04-06 17:53:04 +0200

    Added api sendRestaurantTypes method

--- app/Http/Controllers/Api/DelivebooController.php
+++ app/Http/Controllers/Api/DelivebooController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class DelivebooController extends Controller
 {
+    public function sendRestaurantTypes()
+    {
+        $types = Type::orderBy('name')->get();
+
+        return response()->json(compact('types'));
+    }
 }
