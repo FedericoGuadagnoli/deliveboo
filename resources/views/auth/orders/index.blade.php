e0a40df085ba636461d23dcc43e6b6bf5a27c9a1
AndreaMinelli <minelliandrea94@gmail.com>
2023-04-06 15:33:09 +0200

    Modified orders.index

--- resources/views/auth/orders/index.blade.php
+++ resources/views/auth/orders/index.blade.php
@@ -1,60 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container py-5">
+        <h1 class="text-center text-white">I Tuoi Ordini</h1>
+        <div class="mb-3">
+            <a href="{{ route('dashboard') }}"><i class="fa-solid fa-arrow-left fa-2x text-green"></i></a>
+        </div>
+        {{-- {{ $orders->links() }} --}}
+        <table class="table table-light table-hover">
+            @if (count($orders))
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Cognome</th>
+                        <th scope="col">Indirizzo</th>
+                        <th scope="col">E-Mail</th>
+                        <th scope="col">Telefono</th>
+                        {{-- <th scope="col">Quantità</th> --}}
+                        <th scope="col">Status</th>
+                        <th scope="col">Totale</th>
+                        <th scope="col">Dettagli</th>
+                    </tr>
+                </thead>
+            @endif
+            <tbody>
+                @forelse ($orders as $order)
+                    <tr>
+                        <th class="align-middle pointer" scope="row">{{ $order->id }}</th>
+                        <td class="align-middle pointer">
+                            {{ \Carbon\Carbon::parse($order->created_at)->format('d-m-Y H:i') }}</td>
 
+                        <td class="align-middle pointer">{{ $order->first_name }}</td>
+                        <td class="align-middle pointer">{{ $order->last_name }}</td>
+                        <td class="align-middle pointer ">{{ $order->address }}</td>
+                        <td class="align-middle pointer ">{{ $order->email }}</td>
+                        <td class="align-middle pointer ">{{ $order->phone }}</td>
+                        {{-- @foreach ($order->dishes as $dish)
+                                <td>{{ $dish->pivot->quantity }}</td>
+                                @endforeach --}}
+                        <td class="text-center align-middle pointer">{!! $order->payment_status == 1
+                            ? '<i class="fa-solid fa-check text-success"></i>'
+                            : '<i class="fa-solid fa-xmark text-danger"></i>' !!}</td>
+                        <td class="align-middle pointer">€ {{ $order->total_price }}</td>
+                        <td> <a href="{{ route('admin.orders.show', $order->id) }}" class="btn btn-outline-primary mt-1"><i
+                                    class="fa-solid fa-eye"></i></a></td>
 
-<div class="container py-5">
-    @if  ($orders->count() === 0)
-    <h1 class="text-center text-white mt-5">Non ci sono ordini da mostrare</h1>
-@else
-    <h1 class="text-center text-white">I Tuoi Ordini</h1>
-@endif
-<div class="mb-3">
-    <a href="{{ route('dashboard') }}"><i
-            class="fa-solid fa-arrow-left fa-2x text-green"></i></a>
-</div>
-{{-- {{ $orders->links() }} --}}
-
-    <table class="table table-light table-hover">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Data</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Cognome</th>
-            <th scope="col">Indirizzo</th>
-            <th scope="col">E-Mail</th>
-            <th scope="col">Telefono</th>
-            {{-- <th scope="col">Quantità</th> --}}
-            <th scope="col">Status</th>
-            <th scope="col">Totale</th>
-            <th scope="col">Dettagli</th>
-        </tr>
-        </thead>
-        <tbody>
-            @foreach($orders as $order)
-                <tr>
-                    <th class="align-middle pointer" scope="row">{{ $order->id }}</th>
-                    <td class="align-middle pointer">{{ \Carbon\Carbon::parse($order->created_at)->format('d-m-Y H:i') }}</td>
-
-                    <td class="align-middle pointer">{{ $order->first_name }}</td>
-                    <td class="align-middle pointer">{{ $order->last_name }}</td>
-                    <td class="align-middle pointer ">{{ $order->address }}</td>
-                    <td class="align-middle pointer ">{{ $order->email }}</td>
-                    <td class="align-middle pointer ">{{ $order->phone }}</td>
-                    {{-- @foreach($order->dishes as $dish)
-                    <td>{{ $dish->pivot->quantity }}</td>
-                    @endforeach --}}
-                    <td class="text-center align-middle pointer">{!! $order->payment_status == 1 ? '<i class="fa-solid fa-check text-success"></i>' : '<i class="fa-solid fa-xmark text-danger"></i>' !!}</td>
-                    <td class="align-middle pointer">€ {{ $order->total_price }}</td>
-                    <td> <a href="{{ route('admin.orders.show', $order->id) }}" class="btn btn-outline-primary mt-1"><i class="fa-solid fa-eye"></i></a></td>
-                        
-                </tr>
-            @endforeach
-        </tbody>
-  </table>
-  {{ $orders->links() }}
-</div>
+                    </tr>
+                @empty
+                    <h1 class="text-center text-white mt-5">Non ci sono ordini da mostrare</h1>
+                @endforelse
+            </tbody>
+        </table>
+        {{ $orders->links() }}
+    </div>
 
 
 
