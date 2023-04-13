ea87e51317031a7a0a7139c9a58ef813b9f29e43
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-04-13 10:49:48 +0200

    Adj Api dishes

--- app/Http/Controllers/Api/DelivebooController.php
+++ app/Http/Controllers/Api/DelivebooController.php
@@ -66,13 +66,13 @@ class DelivebooController extends Controller
 
     public function sendRestaurantDishes($slug)
     {
-        $restaurant= Restaurant::select('id')->where('slug',$slug)->first();
+        $restaurant= Restaurant::where('slug',$slug)->first();
        
         $dishes = Dish::where('restaurant_id', $restaurant->id)
               ->where('availability', 1)
               ->get(['id', 'name', 'description', 'price', 'availability', 'image']);
  
-        return response()->json(compact('dishes'));
+        return response()->json(compact('dishes', 'restaurant'));
     }
 
     private function my_in_array($search, $source)
